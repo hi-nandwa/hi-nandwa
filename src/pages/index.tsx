5942a52e6d20bcce07174c9bf657e22a5ff7d58e
ehn <hi@nandwa.dev>
2022-04-27 15:18:06 +0300

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 import Head from "next/head";
 import {NextPage} from "next";
 
-const Home : NextPage = ( ) => {
-    return(
+const Home: NextPage = () => {
+    return (
         <div className="flex min-h-screen  bg-primary">
             <Head>
-                <title>EHN  | Homepage</title>
-                <meta name="description" content="" />
-                <link rel="icon" href="/mylogo.png" />
+                <title>EHN | Homepage</title>
+                <meta name="description" content=""/>
+                <link rel="icon" href="/mylogo.png"/>
             </Head>
 
         </div>
